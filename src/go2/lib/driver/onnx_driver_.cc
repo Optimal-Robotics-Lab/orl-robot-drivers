5a87b20ee3b60e7dc3818f421e5d6e8f50aa7467
jeh15 <jeh15@fsu.edu>
2025-10-29 17:50:52 -0400

    Separate ONNX Logic and Policy Logic. Make Policy Specific implementations.

---
+++ src/go2/lib/driver/onnx_driver_.cc
@@ -0,0 +1,137 @@
+#include "src/go2/lib/driver/onnx_driver.h"
+
+#include <iostream>
+#include <vector>   
+#include <array>
+#include <filesystem>
+#include <thread>
+#include <cmath>
+#include <numeric>
+
+#include "absl/status/status.h"
+#include "absl/log/absl_check.h"
+
+#include <onnxruntime_cxx_api.h>
+
+#include "Eigen/Dense"
+
+
+namespace {
+    template <typename T>
+    T vector_product(const std::vector<T>& v) {
+        return std::accumulate(v.begin(), v.end(), 1, std::multiplies<T>());
+    }
+}
+
+
+ONNXDriver::ONNXDriver(
+    std::filesystem::path onnx_model_path,
+) : 
+    onnx_model_path(onnx_model_path) { }
+
+ONNXDriver::~ONNXDriver() { }
+
+absl::Status ONNXDriver::initialize() {
+    absl::Status result;
+    // Initialize ONNX Session:
+    if (!session_initialized)
+        result.Update(initialize_session());
+
+    ABSL_CHECK(result.ok()) << result.message();
+
+    initialized = true;
+
+    return result;
+};
+
+absl::Status ONNXDriver::initialize_session() {
+    Ort::SessionOptions session_options;
+    session_options.SetIntraOpNumThreads(1);
+    session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
+    session_ptr = std::make_unique<Ort::Session>(*env, onnx_model_path.c_str(), session_options);
+    if (!session_ptr) {
+        return absl::InternalError("Policy Interface: Failed to create ONNX session");
+    }
+
+    // Initialize Inputs and Outputs:
+    for (size_t i = 0; i < session_ptr->GetInputCount(); ++i) {
+        input_nodes.push_back(session_ptr->GetInputNameAllocated(i, allocator));
+        input_names.push_back(input_nodes.back().get());
+        Ort::TypeInfo type_info = session_ptr->GetInputTypeInfo(i);
+        auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
+        input_types.push_back(tensor_info.GetElementType());
+        input_shapes.push_back(tensor_info.GetShape());
+    }
+
+    // Get Output Names and Shapes
+    for (size_t i = 0; i < session_ptr->GetOutputCount(); ++i) {
+        output_nodes.push_back(session_ptr->GetOutputNameAllocated(i, allocator));
+        output_names.push_back(output_nodes.back().get());
+        Ort::TypeInfo type_info = session_ptr->GetOutputTypeInfo(i);
+        auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
+        output_types.push_back(tensor_info.GetElementType());
+        output_shapes.push_back(tensor_info.GetShape());
+    }
+    
+    // Initialize Input and Output Vectors: (assumes 1 input and 1 output tensor)
+    if (input_shapes.size() != 1 || output_shapes.size() != 1) {
+        return absl::InternalError("Policy Interface: Expected 1 input and 1 output tensor");
+    }
+    input_tensor_size = vector_product(input_shapes[0]);
+    output_tensor_size = vector_product(output_shapes[0]);
+    policy_input.resize(input_tensor_size);
+    policy_output.resize(output_tensor_size);
+    observation.resize(input_tensor_size);
+
+    session_initialized = true;
+    return absl::OkStatus();
+};
+
+absl::Status ONNXDriver::set_observation(const Eigen::Vector<float, Eigen::Dynamic>& new_observation) {
+    if (new_observation.size() != observation.size()) {
+        return absl::InvalidArgumentError("Observation size mismatch");
+    }
+
+    observation = new_observation;
+
+    return absl::OkStatus();
+};
+
+absl::Status ONNXDriver::inference_policy() {
+    // Initialize Input and Output Tensors:
+    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(
+        OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault
+    );
+    Ort::Value input_tensor = Ort::Value::CreateTensor<float>(
+        memory_info,
+        policy_input.data(),
+        policy_input.size(),
+        input_shapes[0].data(),
+        input_shapes[0].size()
+    );
+    Ort::Value output_tensor = Ort::Value::CreateTensor<float>(
+        memory_info,
+        policy_output.data(),
+        policy_output.size(),
+        output_shapes[0].data(),
+        output_shapes[0].size()
+    );
+
+    // Inference:
+    Ort::RunOptions run_options;
+    session_ptr->Run(
+        run_options,
+        input_names.data(),
+        &input_tensor,
+        1,
+        output_names.data(),
+        &output_tensor,
+        1
+    );
+
+    if(!output_tensor.HasValue()) {
+        return absl::InternalError("Failed to get output tensor");
+    }
+
+    return absl::OkStatus();
+};
---
+++ src/go2/lib/policies/walking_policy.h
@@ -0,0 +1,238 @@
+#pragma once
+
+#include <iostream>
+#include <vector>   
+#include <array>
+#include <filesystem>
+#include <thread>
+#include <cmath>
+#include <numeric>
+
+#include "absl/status/status.h"
+#include "absl/log/absl_check.h"
+
+#include <onnxruntime_cxx_api.h>
+
+#include "Eigen/Dense"
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "src/utils/constants.h"
+#include "src/go2/lib/utils/constants.h"
+#include "src/go2/lib/driver/go2_driver.h"
+#include "src/go2/msgs/unitree_go_msgs.h"
+
+using Go2State = unitree_go::msg::LowState;
+using Go2Command = unitree_go::msg::LowCmd;
+using namespace robot;
+using namespace robot::constants;
+
+
+/**
+ * @class WalkingPolicy
+ * @brief A class to control the Unitree's Go2 robot using a Policy loaded from an ONNX file.
+ */
+class WalkingPolicy : public rclcpp::Node {
+    public:
+        /**
+         * @brief Construct a new Policy Interface object
+         */
+        WalkingPolicy(
+            std::filesystem::path onnx_model_path,
+            std::shared_ptr<Go2Driver> unitree_driver
+        );
+
+        /**
+         * @brief Destroy the Policy Interface object, ensuring proper cleanup of resources.
+         */
+        ~WalkingPolicy();
+
+        // Disable copy and move semantics to prevent accidental duplication
+        WalkingPolicy(const WalkingPolicy&) = delete;
+        WalkingPolicy& operator=(const WalkingPolicy&) = delete;
+        WalkingPolicy(WalkingPolicy&&) = delete;
+        WalkingPolicy& operator=(WalkingPolicy&&) = delete;
+
+        /**
+         * @brief Initializes the Unitree Driver and ONNX Session.
+         * @return absl::Status OkStatus on success, or an error status on failure.
+         */
+        absl::Status initialize();
+
+        /**
+         * @brief Gets the latest state of the robot.
+         * @return absl::Status OkStatus on success, or an error status if the state cannot be retrieved.
+         */
+        absl::Status get_initial_position();
+
+        /**
+         * @brief Starts the ROS 2 executor in a separate thread to handle callbacks.
+         * @return absl::Status OkStatus on success, or an error status if already spinning.
+         */
+        absl::Status initialize_thread();
+
+        /**
+         * @brief Stops the ROS 2 executor thread.
+         * @return absl::Status OkStatus on success, or an error if the thread cannot be joined.
+         */
+        absl::Status stop_thread();
+
+        /**
+         * @brief Switches the control mode: Default Position, Hold Position, Damping, and Policy.
+         * @param mode The new control mode to set.
+         * @return absl::Status OkStatus on success, or an error if the thread cannot be joined.
+         */
+        absl::Status set_control_mode(go2::constants::HighLevelControlMode mode) {
+            std::lock_guard<std::mutex> lock(mutex);
+            control_mode = mode;
+            return absl::OkStatus();
+        };
+
+        /**
+         * @brief Gets the current control mode.
+         * @return HighLevelControlMode The current control mode.
+         */
+        const go2::constants::HighLevelControlMode control_mode() const {
+            std::lock_guard<std::mutex> lock(mutex);
+            return control_mode;
+        };
+
+        /**
+         * @brief Sets the command to be sent to the robot using an Eigen Vector.
+         * @param new_command The new command to send as a Eigen Vector.
+         * @return absl::Status OkStatus on success, or an error if the command is invalid.
+         */
+        absl::Status set_command(const Vector3<float>& new_command) {
+            std::lock_guard<std::mutex> lock(mutex);
+            command = new_command;
+            return absl::OkStatus();
+        };
+        
+        /**
+         * @brief Sets the command to be sent to the robot using a std::array.
+         * @param new_command The new command to send as an array of floats.
+         * @return absl::Status OkStatus on success, or an error if the command is invalid.
+         */
+        absl::Status set_command(const std::array<float, 3>& new_command) {
+            std::lock_guard<std::mutex> lock(mutex);
+            command = Eigen::Map<const constants::Vector3<float>>(new_command.data());
+            return absl::OkStatus();
+        };
+
+        /**
+         * @brief Gets the current command being sent to the robot.
+         * @return Vector3<float> The current command.
+         */
+        const Vector3<float> command() const {
+            std::lock_guard<std::mutex> lock(mutex);
+            return command;
+        };
+
+        /**
+         * @brief Sets the master gain that scales the policy output.
+         * @param gain The new master gain value.
+         * @return absl::Status OkStatus on success, or an error if the gain is invalid.
+         */
+        absl::Status set_master_gain(float gain) {
+            std::lock_guard<std::mutex> lock(mutex);
+            master_gain = std::clamp(gain, 0.0f, 1.0f);
+            if (master_gain < 0.0f || master_gain > 1.0f) {
+                return absl::InvalidArgumentError("Policy Interface [set_master_gain]: Gain clamped between 0.0 and 1.0.");
+            }
+            return absl::OkStatus();
+        };
+
+
+        /**
+         * @brief Gets the current master gain.
+         * @return float The current master gain value.
+         */
+        const float master_gain() const {
+            std::lock_guard<std::mutex> lock(mutex);
+            return master_gain;
+        }
+
+        /**
+         * @brief Gets the output of the policy.
+         * @return std::vector<float> The current output of the policy.
+         */
+        const std::vector<float> policy_output() const {
+            std::lock_guard<std::mutex> lock(mutex);
+            return onnx_driver->policy_output();
+        }
+
+        /**
+         * @brief Gets the current observation.
+         * @return Eigen::Vector<float, Eigen::Dynamic> The current observation.
+         */
+        const Eigen::Vector<float, Eigen::Dynamic> observation() const {
+            std::lock_guard<std::mutex> lock(mutex);
+            return onnx_driver->observation();
+        }
+
+        /**
+         * @brief Checks if the ONNX session has been initialized.
+         * @return true if the ONNX session is initialized, false otherwise.
+         */
+        const bool is_initialized() const {
+            return initialized;
+        }
+
+        /**
+         * @brief Checks if the ROS 2 executor thread has been initialized.
+         * @return true if the executor thread is initialized, false otherwise.
+         */
+        const bool is_thread_initialized() const {
+            return thread_initialized;
+        }
+    
+    private:
+        /**
+         * @brief Constructs a new observation from the Unitree driver.
+         * @return absl::Status OkStatus on success, or an error status on failure.
+         */
+        absl::Status make_observation();
+
+        /**
+         * @brief Transforms the Policy output to a Go2 Command.
+         * @return Go2Command Motor command.
+         */
+        Go2Command policy_command();
+
+        /**
+         * @brief Callback function for the ROS 2 executor to handle policy updates.
+         */
+        void policy_callback();
+
+        // Shared Variables
+        constants::Vector3<float> command = { 0.0, 0.0, 0.0 };
+
+        // ONNX Variables
+        std::filesystem::path onnx_model_path;
+        std::shared_ptr<ONNXDriver> onnx_driver = std::make_shared<ONNXDriver>(onnx_model_path);
+        
+        // Initialization Flags
+        bool thread_initialized = false;
+        bool initialized = false;
+        
+        // Unitree Driver
+        std::shared_ptr<Go2Driver> unitree_driver;
+        
+        // Thread Variables
+        std::mutex mutex;
+
+        // ROS2 Thread:
+        rclcpp::executors::MultiThreadedExecutor executor;
+        std::jthread executor_thread;
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        // Default Command Values
+        go2::constants::HighLevelControlMode control_mode = go2::constants::HighLevelControlMode::DAMPING;
+        int control_rate_us;
+        const float action_scale = 0.5f;
+        float master_gain = 0.0f;
+        const go2::constants::MotorVector<float> default_position = Eigen::Map<const go2::constants::MotorVector<float>>(go2::constants::default_position.data());
+        static constexpr std::array<float, go2::constants::num_joints> kp = go2::constants::default_kp;
+        static constexpr std::array<float, go2::constants::num_joints> kd = go2::constants::default_kd;
+
+};
